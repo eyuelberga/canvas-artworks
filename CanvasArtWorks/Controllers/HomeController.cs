8c4873c8ae58ca51fb3b8cf26e99f462b6f67d71
eyuelberga <eyuel.berga@yahoo.com>
2019-12-28 06:57:45 +0300

    responsive svgs

--- CanvasArtWorks/Controllers/HomeController.cs
+++ CanvasArtWorks/Controllers/HomeController.cs
@@ -1,22 +1,28 @@
 ﻿using System.Diagnostics;
+using CanvasArtWorks.Data;
 using CanvasArtWorks.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace CanvasArtWorks.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ArtworkContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, ArtworkContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Artworks.ToListAsync());
         }
 
         public IActionResult Privacy()
